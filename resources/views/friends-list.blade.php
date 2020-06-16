a7dd8afc91a4bf6c0258170cf30f5a8c745c4339
Dina Essam <dinaessam@gate7.me>
2020-06-16 11:50:26 +0200

    Add Follows

--- resources/views/friends-list.blade.php
+++ resources/views/friends-list.blade.php
@@ -1,15 +1,15 @@
 <div class=" bg-blue-100 rounded-lg p-4">
     <h3 class="font-bold text-xl mb-4">Friends</h3>
     <ul>
-        @foreach(range(1,8) as $index)
+        @foreach(auth()->user()->follows as $friend)
             <li>
                 <div class="flex items-center text-sm mb-4">
                     <img
-                        src="https://i.pravatar.cc/40"
+                        src={{$friend->getAvatarAttribute(40)}}
                         alt=""
                         class="rounded-full mr-2"
                     >
-                    Dina Essam
+                    {{$friend->name}}
                 </div>
             </li>
         @endforeach
