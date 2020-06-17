91fa88c739933f3274420a8517d3550c386630ab
Dina Essam <dinaessam@gate7.me>
2020-06-17 15:17:03 +0200

    Add logout

--- resources/views/sidebar-links.blade.php
+++ resources/views/sidebar-links.blade.php
@@ -1,42 +1,45 @@
 <ul>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="{{route('home')}}">
+        <a class="font-bold text-lg mb-4 block hover:text-blue-300" href="{{route('home')}}">
             Home
         </a>
     </li>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="{{route('explore')}}">
+        <a class="font-bold text-lg mb-4 block hover:text-blue-300" href="{{route('explore')}}">
             Explore
         </a>
     </li>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="/">
+        <a class="font-bold text-lg mb-4 block hover:text-blue-300" href="/">
             Notifications
         </a>
     </li>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="/">
+        <a class="font-bold text-lg mb-4 block hover:text-blue-300" href="/">
             Messages
         </a>
     </li>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="/">
+        <a class="font-bold text-lg mb-4 block hover:text-blue-300" href="/">
             Bookmarks
         </a>
     </li>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="/">
+        <a class="font-bold text-lg mb-4 block hover:text-blue-300" href="/">
             Lists
         </a>
     </li>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="{{route('profile',auth()->user()->username)}}">
+        <a class="font-bold text-lg mb-4 block hover:text-blue-300" href="{{route('profile',auth()->user()->username)}}">
             Profile
         </a>
     </li>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="/">
-            More
-        </a>
+        <form method="POST" action="{{route('logout')}}">
+            @csrf
+            <button class="font-bold text-lg hover:text-blue-300">
+                Logout
+            </button>
+        </form>
     </li>
 </ul>
